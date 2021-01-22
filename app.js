8ad764fb256965e0fee075bc25742e609f131a62
Sunnylawrence <sunnylawrence0609@gmail.com>
2021-01-22 23:40:31 +0530

    first commit

--- app.js
+++ app.js
@@ -2,11 +2,22 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
+
+const userSchema={
+    email:String,
+    password:String
+};
+
+const user = new mongoose.model("user", userSchema);
+
 
 app.get("/",(req, res)=>{
     res.render("home");
@@ -16,6 +27,24 @@ app.get("/login",(req, res)=>{
 });
 app.get("/register",(req, res)=>{
     res.render("register");
+});
+
+
+
+app.post("/register", (req, res)=>{
+    const newUser = new user({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("main-page");
+        }
+       
+    });
 });
 
 app.listen(3000, ()=>{
